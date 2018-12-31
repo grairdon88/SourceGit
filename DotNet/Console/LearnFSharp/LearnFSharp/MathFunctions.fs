eb19910f5fd82b387a8a7fe8af3f6d4fca0ed92c
Greg Rairdon <grairdon88@gmail.com>
2018-12-30 20:45:17 -0500

    Initial commit.

---
+++ DotNet/Console/LearnFSharp/LearnFSharp/MathFunctions.fs
@@ -0,0 +1,20 @@
+﻿module MathFunctions
+
+open System
+
+let multiply num multiplier  =
+    num * multiplier
+
+let divide numerator denominator =
+
+    if denominator = 0
+        then 0
+    else
+        numerator / denominator
+
+let raisetopower (number : int32) (powernumber : int32) =
+    pown number powernumber
+
+let rec FibonacciSequence n =
+    if n < 2 then 1
+    else FibonacciSequence (n - 1) + FibonacciSequence (n - 2)
---
+++ DotNet/Console/LearnFSharp/LearnFSharp/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+open MathFunctions
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Hello World from F#!")
+    Console.WriteLine("{0}", multiply 5 2)
+    Console.WriteLine("{0}", divide 1 0)
+    Console.WriteLine("{0}", raisetopower 10 2)
+    for i = 1 to 10 do
+       Console.WriteLine("Fibonacci {0}: {1}", i, FibonacciSequence i)
+    Console.ReadKey()
+    0 // return an integer exit code
